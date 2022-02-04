924bd97f9b915e0f0efa081e846192b4ce05f255
Marcelo <snaker.extreme.15@gmail.com>
2022-02-04 14:00:36 -0300

    [Develop] <Marcelo> Add listar_promedionotas function to views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from django.shortcuts import render
 from core.models import Alumno, Alumno_Curso, Alumno_Curso_Prueba, Profesor, Prueba, Curso
 
@@ -7,22 +8,39 @@ from core.models import Alumno, Alumno_Curso, Alumno_Curso_Prueba, Profesor, Pru
 def mostrar_alumnos(request):
     alumnos = Alumno.objects.all().order_by("nombre")
     context = {"alumnos": alumnos}
-    return render(request, 'core/alumnos.html', context)
+    return render(request, 'alumnos.html', context)
 
 
 def mostrar_cursos(request):
     cursos = Curso.objects.all().order_by("nombre_curso")
     context = {"cursos": cursos}
-    return render(request, 'core/cursos.html', context)
+    return render(request, 'cursos.html', context)
 
 
 def mostrar_pruebas(request):
     pruebas = Prueba.objects.all().order_by("nombre_prueba")
     context = {"pruebas": pruebas}
-    return render(request, 'core/pruebas.html', context)
+    return render(request, 'pruebas.html', context)
 
 
 def mostrar_notas(request):
-    notas = Alumno_Curso_Prueba.objects.all()
+    notas = Alumno_Curso.objects.get_or_create()
+    notas2 = notas.alumno_curso_prueba_set.all()
     context = {"notas": notas}
-    return render(request, 'core/notas.html', context)
+    return render(request, 'notas.html', context)
+
+
+def listar_promedionotas(request):
+    #todaslasnotasynombres = Alumno_Curso_Prueba.objects.all().order_by("nota")
+    # notass = Alumno_Curso_Prueba.objects.filter(
+    #    nota__lte=4, alumno_curso__alumno__nombre="Adam")
+    # notass = Alumno_Curso_Prueba.objects.filter(
+    #    nota__lte=4)
+
+    #notasss = Alumno_Curso_Prueba.objects.filter(nota__lte=4)
+    querysett = Alumno_Curso_Prueba.objects.values(
+        "alumno_curso__alumno__nombre", "alumno_curso__curso__nombre_curso").annotate(Avg("nota"))
+    queryset = list(querysett)
+    context = {"queryset": queryset}
+
+    return render(request, 'listarpromedionotas.html', context)
