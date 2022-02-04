4099650af70569c52090ef4fe082fd99959f290e
Marcelo <snaker.extreme.15@gmail.com>
2022-02-04 13:58:50 -0300

    [Desarrollo] <Marcelo> Modify models adding metaclass and properties,and modify admin to better visualization on admin panel

--- core/admin.py
+++ core/admin.py
@@ -12,9 +12,22 @@ class Profesor_Admin(admin.ModelAdmin):
     search_fields = ("nombre", "apellido")
 
 
+class Prueba_Admin(admin.ModelAdmin):
+    list_display = ("nombre_curso", "nombre_prueba")
+
+
+class Alumno_Curso_Admin(admin.ModelAdmin):
+    list_display = ("id", "nombre_curso", "nombre_alumno")
+
+
+class Alumno_Curso_Prueba_Admin(admin.ModelAdmin):
+    list_display = ("nombre_alumno", "nombre_curso", "nombre_prueba", "nota")
+    list_filter = ("alumno_curso__alumno",)
+
+
 admin.site.register(Alumno, Alumno_Admin)
 admin.site.register(Profesor, Profesor_Admin)
-admin.site.register(Alumno_Curso)
-admin.site.register(Alumno_Curso_Prueba)
+admin.site.register(Alumno_Curso, Alumno_Curso_Admin)
+admin.site.register(Alumno_Curso_Prueba, Alumno_Curso_Prueba_Admin)
 admin.site.register(Curso)
-admin.site.register(Prueba)
+admin.site.register(Prueba, Prueba_Admin)
--- core/models.py
+++ core/models.py
@@ -12,6 +12,9 @@ class Alumno(models.Model):
     def __str__(self):
         return self.nombre
 
+    class Meta:
+        ordering = ['id']
+
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=70)
@@ -22,6 +25,12 @@ class Profesor(models.Model):
     def __str__(self):
         return self.nombre
 
+    class Meta:
+        ordering = ['id']
+
+    class Meta:
+        ordering = ['id']
+
 
 class Curso(models.Model):
     nombre_curso = models.CharField(max_length=100, null=True)
@@ -30,6 +39,9 @@ class Curso(models.Model):
 
     def __str__(self):
         return self.nombre_curso
+
+    class Meta:
+        ordering = ['id']
 
 
 class Alumno_Curso(models.Model):
@@ -41,14 +53,29 @@ class Alumno_Curso(models.Model):
     def __str__(self):
         return str(self.id)
 
+    class Meta:
+        ordering = ['id']
+
+    @property
+    def nombre_curso(self):
+        return self.curso.nombre_curso
+
+    @property
+    def nombre_alumno(self):
+        return self.alumno.nombre
+
 
 class Prueba(models.Model):
     nombre_prueba = models.CharField(max_length=60)
     curso = models.ForeignKey(
         Curso, on_delete=models.SET_NULL, null=True)
 
-    def __str__(self):
-        return self.nombre_prueba
+    @property
+    def nombre_curso(self):
+        return self.curso.nombre_curso
+
+    class Meta:
+        ordering = ['id']
 
 
 class Alumno_Curso_Prueba(models.Model):
@@ -59,4 +86,20 @@ class Alumno_Curso_Prueba(models.Model):
     nota = models.FloatField(blank=True, null=True)
 
     def __str__(self):
-        return f"ID: {str(self.id)} - Alumno: {self.alumno_curso.alumno.nombre} - Prueba: {self.prueba.nombre_prueba} - Nota: {str(self.nota)}"
+        # return f"ID: {str(self.id)} - Alumno: {self.alumno_curso.alumno.nombre} - Prueba: {self.prueba.nombre_prueba} - Nota: {str(self.nota)}"
+        return f"{str(self.nota)} {self.alumno_curso.alumno.nombre}"
+
+    @property
+    def nombre_prueba(self):
+        return self.prueba.nombre_prueba
+
+    @property
+    def nombre_alumno(self):
+        return self.alumno_curso.alumno.nombre
+
+    @property
+    def nombre_curso(self):
+        return self.alumno_curso.curso.nombre_curso
+
+    class Meta:
+        ordering = ['id']
